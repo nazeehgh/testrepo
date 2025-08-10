e974b0850337584b9105b139340cc4b98a1d125c
nazeehgh <nazeehgh@gmail.com>
2025-08-10 10:26:35 +0300

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+function mydatetimefun(){
+    document.getElementById("demo").innerHTML = Date();
+
+}
+
+function myfun02(){
+    // document.getElementById("mydiv1").style.Color = "red";
+    document.getElementById("mydiv1").innerText = "red";
+}
+
+function fun3(){
+    // document.getElementById("mydiv1").innerText = "ABC";
+    document.getElementById("mydiv1").style.fontSize = "35px";
+    document.getElementById("mydiv1").style.color = '#FF0000';
+    document.getElementById("mydiv1").style.background = '#005533';
+    document.getElementById("bt1").style.display  = 'none';
+
+
+}
